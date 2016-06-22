6a4781f3c598aa0aa7f02c24efc443b18df81dcb
Sunny Mistry <sunnymis@buffalo.edu>
2016-06-22 14:03:28 -0500

    added nav module

--- app/app.js
+++ app/app.js
@@ -3,12 +3,13 @@
 // Declare app level module which depends on views, and components
 angular
     .module('tom', [
-        'ngRoute'
+        'ngRoute',
+        'tom.nav'
     ])
     .config(configFunction);
 
 configFunction.$inject = ['$routeProvider'];
 
 function configFunction($routeProvider) {
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.otherwise({redirectTo: '/'});
 }
