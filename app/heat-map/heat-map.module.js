a041d94b1b52f6b4ab181c219027b81bd3601ca2
Sunny Mistry <sunnymis@buffalo.edu>
2016-06-23 17:48:18 -0500

    heat map is in the site now

--- app/heat-map/heat-map.module.js
+++ app/heat-map/heat-map.module.js
@@ -1,6 +1,49 @@
 (function() {
     'use strict';
 
-    angular
+    var heatMap = angular
         .module('tom.heat-map',[]);
-})();
+
+    heatMap.controller('HeatMapController', function createHeatMap($scope, $http) {
+      $http.get('http://tomkennedy.herokuapp.com/api/cousins').success(function(data) {
+          google.charts.load('current', {'packages':['geomap']});
+          google.charts.setOnLoadCallback(drawMap);
+
+          const aspectRatio = 1.6023054755; // width/height
+          const height = 700; //px
+          var cousins = data;
+
+          function drawMap() {
+            var data = google.visualization.arrayToDataTable(
+              _.concat([['Country', 'Percent']],
+                _.map(cousins, function(countryData) {
+                  return [countryData.country, countryData.percent];
+                })
+              )
+            );
+
+            var options = {};
+            options['dataMode'] = 'regions';
+            options['width'] = height*aspectRatio + 'px';
+            options['height'] = height + 'px';
+
+            var container = document.getElementById('regions_div');
+            var geomap = new google.visualization.GeoMap(container);
+
+            geomap.draw(data, options);
+          };
+        });
+      }
+    )
+    
+    .directive('hmDir', hmDir)
+    
+    function hmDir() {
+        return {
+            templateUrl: 'heat-map/heat-map.html',
+            restrict: 'E',
+            scope: {}
+        };
+    }
+  }
+)();
